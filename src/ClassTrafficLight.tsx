3bd524db55cf4a7a9bf1a02c6dc6c88d2c41bd84
Shupermokey <a.e.sobieraj@gmail.com>
2024-01-03 10:30:59 -0600

    traffic light state: functional and class

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,40 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  state = {
+    lightColor: ["red", "yellow", "green", "black"],
+    activeColor: 0,
+    inactiveColor: 3,
+  };
+
+  changeColor = () => {
+    if (this.state.activeColor < 2) {
+      this.setState({ activeColor: this.state.activeColor + 1 });
+    } else {
+      this.setState({ activeColor: 0 });
+    }
+  };
+
+  returnLightColor = (num: number) => {
+    return `circle ${
+      this.state.activeColor % 3 === num
+        ? this.state.lightColor[this.state.activeColor]
+        : this.state.lightColor[this.state.inactiveColor]
+    }`;
+  };
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={this.returnLightColor(0)}></div>
+          <div className={this.returnLightColor(1)}></div>
+          <div className={this.returnLightColor(2)}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button onClick={this.changeColor} className="next-state-button">
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  const [lightColor, setLightColor] = useState([
+    "red",
+    "yellow",
+    "green",
+    "black",
+  ]);
+  const [activeColor, setActiveColor] = useState(0);
+  const [inactiveColor, setInctiveColor] = useState(3);
+
+  function changeColor() {
+    if (activeColor < 2) {
+      setActiveColor(activeColor + 1);
+    } else {
+      setActiveColor(0);
+    }
+  }
+
+  function returnLightColor(num: number) {
+    return `circle ${
+      activeColor % 3 === num
+        ? lightColor[activeColor]
+        : lightColor[inactiveColor]
+    }`;
+  }
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={returnLightColor(0)}></div>
+        <div className={returnLightColor(1)}></div>
+        <div className={returnLightColor(2)}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button onClick={changeColor} className="next-state-button">
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
